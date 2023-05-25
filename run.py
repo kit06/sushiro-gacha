c3d9197287068f8cd10dd88451d33c812bb1b028
kit06 <hts6573@gmail.com>
2023-05-25 23:55:12 +0900

    UPDATE:制御コードに変更

--- run.py
+++ run.py
@@ -1,22 +1,7 @@
-from flask import Flask, request, redirect, render_template, session, flash
+from flask import Flask, request, redirect, render_template, flash
+from app import create_app
 
-app = Flask(__name__)
-
-#寿司の値段：1,000円以内
-@app.route('/matsu')
-def matsu():
-  return render_template('matsu.html')
-
-#寿司の値段：3,000円以内
-@app.route('take')
-def take():
-    return render_template('take.html')
-
-#寿司の値段：5,000円以内
-@app.route('ume')
-def ume():
-    return render_template('ume.html')
-
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
