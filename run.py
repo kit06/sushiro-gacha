729191328a20a7d5bfcea427440fb80f13e552a5
kit06 <hts6573@gmail.com>
2023-05-28 18:30:10 +0900

    ADD:load_doenvを追記

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, redirect, render_template, flash
 from app import create_app
+import app.controllers
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = create_app()
 
