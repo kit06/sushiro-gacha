8bd13f1279d67419b799cf41adbc2accb5ed80ce
kit06 <hts6573@gmail.com>
2023-05-28 18:44:02 +0900

    UPDATE:generate_gacha_resultに最低価格と合計価格の出力機能を追記

--- app/controllers/sushi_controller.py
+++ app/controllers/sushi_controller.py
@@ -1,27 +1,33 @@
 from flask import Blueprint, redirect, render_template, request
-from models import get_sushi_data
+from ..models.models import Db
 
 sushi_controller_bp = Blueprint('sushi_controller', __name__)
 
 @sushi_controller_bp.route('/')
 def gacha():
-    return redirect('/gacha/sushi')
+    return redirect('/sushi-gacha')
 
-@sushi_controller_bp.route('/gacha/sushi', methods=['GET', 'POST'])
+@sushi_controller_bp.route('/sushi-gacha', methods=['GET', 'POST'])
 def submit_result():
     if request.method == 'POST':
-        sushi = request.form.get('sushi')
+        budget = int(request.form.get('budget'))
+        # ガチャの結果を生成する処理
+        result, total_price = generate_gacha_result(budget)
+        # 結果を画面に返す
+        return render_template('result.html', result=result, total_price=total_price,budget=budget)
+    else:
+        return render_template('sushi-gacha.html')
 
-        if sushi == '1':
-            # ガチャの結果を生成する処理
-            result = generate_gacha_result()
-
-            # 結果を画面に返す
-            return render_template('sushi-gacha.html', sushi=result)
-
-    return render_template('sushi-gacha.html')
-
-# TODO: ビジネスロジックの実装
-def generate_gacha_result():
-    sushi = get_sushi_data()
-    # TODO: ガチャの処理
+def generate_gacha_result(budget):
+    db = Db()
+    # 最低価格の取得
+    min_price = db.calc_min_price()
+    # 合計価格と結果の宣言
+    total_price = 0
+    result = []
+    # 予算 - 現在のガチャ結果の合計金額が最低価格を下回るまでループ
+    while budget - total_price >= min_price:
+        sushi = db.get_random_sushi_data_within_budget(budget - total_price)
+        result.append(sushi)
+        total_price += sushi.price
+    return result, total_price
