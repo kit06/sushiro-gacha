6a045324b6197c3b0d7892a7f06f754e2a09c3fa
kit06 <hts6573@gmail.com>
2023-06-04 17:20:44 +0900

    UPDATE:DBからの取得ロジックの変更

--- app/controllers/sushi_controller.py
+++ app/controllers/sushi_controller.py
@@ -7,6 +7,10 @@ sushi_controller_bp = Blueprint('sushi_controller', __name__)
 def gacha():
     return redirect('/sushi-gacha')
 
+@sushi_controller_bp.route('/again-gacha', methods=['POST'])
+def again_gacha():
+    return redirect('/sushi-gacha')
+
 @sushi_controller_bp.route('/sushi-gacha', methods=['GET', 'POST'])
 def submit_result():
     if request.method == 'POST':
@@ -20,14 +24,17 @@ def submit_result():
 
 def generate_gacha_result(budget):
     db = Db()
-    # 最低価格の取得
-    min_price = db.calc_min_price()
-    # 合計価格と結果の宣言
-    total_price = 0
-    result = []
-    # 予算 - 現在のガチャ結果の合計金額が最低価格を下回るまでループ
-    while budget - total_price >= min_price:
-        sushi = db.get_random_sushi_data_within_budget(budget - total_price)
-        result.append(sushi)
-        total_price += sushi.price
+    # # 最低価格の取得
+    # min_price = db.calc_min_price()
+    # # 合計価格と結果の宣言
+    # total_price = 0
+    # result = []
+    # # 予算 - 現在のガチャ結果の合計金額が最低価格を下回るまでループ
+    # while budget - total_price >= min_price:
+    #     sushi = db.get_random_sushi_data_within_budget(budget - total_price)
+    #     result.append(sushi)
+    #     total_price += sushi.price
+
+    result = db.get_sushi_datas_within_budget(budget)
+    total_price = sum(sushi.price for sushi in result)
     return result, total_price
